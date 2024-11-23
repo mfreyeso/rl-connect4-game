633b77f0e61d5417cfdf3b86a3b845bb4c3955c2
Mario <m.reyeso@uniandes.edu.co>
2024-11-23 13:15:47 -0500

    added more mdp code

--- connect4/constants.py
+++ connect4/constants.py
@@ -1,6 +1,18 @@
+from typing import Final
+
 N_ROWS = 6
 N_COLS = 7
 
 SQUARE_SIZE = 100
 WIDTH = N_COLS * SQUARE_SIZE
 HEIGHT = (N_ROWS + 1) * SQUARE_SIZE
+
+ACTION_BLOCK: Final[str] = "block"
+ACTION_DROP: Final[str] = "drop"
+ACTION_DROP_WIN: Final[str] = "drop_win"
+
+
+def get_reward(action: str) -> int:
+    rewards_map = {ACTION_BLOCK: 100, ACTION_DROP: 10, ACTION_DROP_WIN: 500}
+
+    return rewards_map[action]
--- connect4/environment.py
+++ connect4/environment.py
@@ -1,7 +1,16 @@
 import random
+from typing import Any, Optional
+
 import numpy as np
 
-from connect4.constants import N_ROWS, N_COLS
+from connect4.constants import (
+    N_ROWS,
+    N_COLS,
+    ACTION_DROP,
+    ACTION_BLOCK,
+    ACTION_DROP_WIN,
+    get_reward,
+)
 
 
 class Connect4Environment:
@@ -43,17 +52,14 @@ class Connect4Environment:
     def state_value(self, state: tuple[int, int]):
         return self._board[state[0]][state[1]]
 
-    def get_reward(self, state: tuple[int, int], piece: int) -> int:
-        # todo: our reward based on the state reached
-        #  ~ analyze if we block or align one more pos to shape the 4.
-        if self._board:
-            return 0
-
-    def get_possible_actions(self, state: tuple[int, int], piece: int) -> list[tuple[int, int]]:
+    def get_possible_positions(
+        self,
+    ) -> tuple[list[tuple[int, int]], dict[tuple[int, int], Any]]:
         available_positions = []
         filled_positions = {}
         temp_filled_cols = []
 
+        current_row = 0
         for row in range(N_ROWS):
             for col in range(N_COLS):
                 if self._board[row][col] == 0:
@@ -63,19 +69,37 @@ class Connect4Environment:
                     temp_filled_cols.append(col)
 
             if len(temp_filled_cols) != N_COLS:
+                current_row = row
                 break
 
         for col in temp_filled_cols:
-            for row in range(N_ROWS):
-                if self._board[row][col] != 0:
-                    filled_positions[(row, col)] = self.board[row][col]
-                else:
-                    available_positions.append((row, col))
-                    break
+            available_positions.append((current_row + 1, col))
 
-        # todo: we could sample the available options based on state and piece.
+        return available_positions, filled_positions
 
-        return available_positions
+    def get_possible_actions(self, piece: int) -> list[str]:
+        available_positions, filled_positions = self.get_possible_positions()
+
+        # adversary filled positions
+        con_filled_positions = {
+            state: piece_i
+            for state, piece_i in filled_positions.items()
+            if piece_i != piece
+        }
+
+        if len([con_filled_positions]) < 3:
+            # early steps in game, no required block yet
+            actions = [ACTION_DROP]
+        else:
+            adv_piece = next(iter(con_filled_positions.values()))
+            assert adv_piece != piece
+
+            if self.winning_move(adv_piece):
+                actions = [ACTION_BLOCK]
+            else:
+                actions = [ACTION_DROP]
+
+        return actions
 
     def is_terminal(self, player_1: int, player_2: int):
         return self.winning_move(player_1) or self.winning_move(player_2)
@@ -88,6 +112,29 @@ class Connect4Environment:
             if self._board[r][col] == 0:
                 return r
 
+    def do_action(self, action: str, piece: int) -> tuple[tuple[int, int], int]:
+        available_positions, filled_positions = self.get_possible_positions()
+        adv_piece = next(iter(filled_positions.values()))
+
+        is_final = self.is_terminal(piece, adv_piece)
+
+        if action == ACTION_DROP:
+            row, col = (
+                self.winner_position(piece)
+                if is_final
+                else random.choice(available_positions)
+            )
+            action = ACTION_DROP_WIN
+        else:
+            row, col = self.winner_position(adv_piece)
+
+        reached_stated = (row, col)
+
+        self._board[row][col] = piece
+        self.state = reached_stated
+
+        return reached_stated, get_reward(action)
+
     def drop_piece(self, row, col, piece):
         # todo: replace later with drop_piece_reward
         self._board[row][col] = piece
@@ -98,51 +145,56 @@ class Connect4Environment:
         self._board[row][col] = piece
         self.state = reached_stated
 
-        return reached_stated, self.get_reward(reached_stated, piece)
+        # return reached_stated, self.get_reward(reached_stated, piece)
+        # return reached_stated, self.get_reward(reached_stated, piece)
+        return
 
     def winning_move(self, piece) -> bool:
+        return bool(self.winner_position(piece))
+
+    def winner_position(self, piece) -> Optional[tuple[int, int]]:
         # revisando las posiciones horizontales
         for c in range(N_COLS - 3):
             for r in range(N_ROWS):
                 if (
-                        self._board[r][c] == piece
-                        and self._board[r][c + 1] == piece
-                        and self._board[r][c + 2] == piece
-                        and self._board[r][c + 3] == piece
+                    self._board[r][c] == piece
+                    and self._board[r][c + 1] == piece
+                    and self._board[r][c + 2] == piece
+                    and self._board[r][c + 3] == piece
                 ):
-                    return True
+                    return r, c
 
         # verificando las posiciones verticales
         for c in range(N_COLS):
             for r in range(N_ROWS - 3):
                 if (
-                        self._board[r][c] == piece
-                        and self._board[r + 1][c] == piece
-                        and self._board[r + 2][c] == piece
-                        and self._board[r + 3][c] == piece
+                    self._board[r][c] == piece
+                    and self._board[r + 1][c] == piece
+                    and self._board[r + 2][c] == piece
+                    and self._board[r + 3][c] == piece
                 ):
-                    return True
+                    return r, c
 
         # verificando diagonales positivas
         for c in range(N_COLS - 3):
             for r in range(N_ROWS - 3):
                 if (
-                        self._board[r][c] == piece
-                        and self._board[r + 1][c + 1] == piece
-                        and self._board[r + 2][c + 2] == piece
-                        and self._board[r + 3][c + 3] == piece
+                    self._board[r][c] == piece
+                    and self._board[r + 1][c + 1] == piece
+                    and self._board[r + 2][c + 2] == piece
+                    and self._board[r + 3][c + 3] == piece
                 ):
-                    return True
+                    return r, c
 
         # verificando diagonales negativas
         for c in range(N_COLS - 3):
             for r in range(3, N_ROWS):
                 if (
-                        self._board[r][c] == piece
-                        and self._board[r - 1][c + 1] == piece
-                        and self._board[r - 2][c + 2] == piece
-                        and self._board[r - 3][c + 3] == piece
+                    self._board[r][c] == piece
+                    and self._board[r - 1][c + 1] == piece
+                    and self._board[r - 2][c + 2] == piece
+                    and self._board[r - 3][c + 3] == piece
                 ):
-                    return True
+                    return r, c
 
-        return False
+        return None
